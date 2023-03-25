cd82ecfe67ed7dac6514aff3bead6ee3c2830bd3
creekwater <zhaoyang_wu@foxmail.com>
2023-03-25 23:42:30 +0800

    删除无用接口

--- api/tcpclient.h
+++ api/tcpclient.h
@@ -18,29 +18,7 @@ public:
 
 private:
     QTcpSocket *socket;
-    QString ip;
-    int port;
-
-public:
-    QString getIP() const;
-    int getPort()   const;
-
-private slots:
-    void slot_disconnected();
-    void slot_error();
-    void slot_readData();
-
-signals:
-    void disconnected(const QString &ip, int port);
-    void error(const QString &ip, int port, const QString &error);
-
-    void sendData(const QString &ip, int port, const QString &data);
-    void receiveData(const QString &ip, int port, const QString &data);
-
-public slots:
-    void sendData(const QString &data);
-    void disconnectFromHost();
-    void abort();
+    
 };
 
 #endif // TCPCLIENT_H
