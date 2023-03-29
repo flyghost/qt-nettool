bb682621d32bb42b953ade99b5bf8f7ff20c19c6
creekwater <zhaoyang_wu@foxmail.com>
2023-03-30 00:28:55 +0800

    修改字体等

--- applications/nettool/main.cpp
+++ applications/nettool/main.cpp
@@ -16,14 +16,6 @@ int main(int argc, char *argv[])
     //设置编码+字体+中文翻译文件
     QUIHelper::initAll();
 
-    //读取配置文件
-    AppConfig::ConfigFile = QString("%1/%2.ini").arg(QUIHelper::appPath()).arg(QUIHelper::appName());
-    AppConfig::readConfig();
-
-    AppData::Intervals << "1" << "10" << "20" << "50" << "100" << "200" << "300" << "500" << "1000" << "1500" << "2000" << "3000" << "5000" << "10000";
-    AppData::readSendData();
-    AppData::readDeviceData();
-
     frmMain w;
     w.setWindowTitle("网络调试助手 V2022 (copyright by creekwater)");
     w.resize(950, 700);
--- examples/frmmain/frmmain.cpp
+++ examples/frmmain/frmmain.cpp
@@ -13,6 +13,14 @@
 
 frmMain::frmMain(QWidget *parent) : QWidget(parent), ui(new Ui::frmMain)
 {
+    //读取配置文件
+    AppConfig::ConfigFile = QString("%1/%2.ini").arg(QUIHelper::appPath()).arg(QUIHelper::appName());
+    AppConfig::readConfig();
+
+    AppData::Intervals << "1" << "10" << "20" << "50" << "100" << "200" << "300" << "500" << "1000" << "1500" << "2000" << "3000" << "5000" << "10000";
+    AppData::readSendData();
+    AppData::readDeviceData();
+
     ui->setupUi(this);
     this->initForm();
     this->initConfig();
